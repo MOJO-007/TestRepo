fa8124eaa54a1497e2f16b7c6106d28585324a43
Thanushree R <122381167+ThanuRavi01@users.noreply.github.com>
2023-05-04 16:31:52 +0530

    Update hello.java
    Changed value of a

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
 public class hello {
 
     public static void main(String[] args) {
-        int a=99999900;
+        int a=9;
         System.out.println("This was a file for test repo");
-        //This line was added by ambika!
-    }
+            }
 }
