dd12805137d495570557e42787ac4e206ea9191e
Varshini-4569 <122376308+Varshini-4569@users.noreply.github.com>
2023-05-05 23:09:35 +0530

    Update hello.java
    b value =9

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
 
     public static void main(String[] args) {
-        int b=99999900;
+        int b=9;
         System.out.println("This was a file for test repo");
     }
 }
