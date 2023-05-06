829a5aaaab086e876026f116c1cf75493e068ac1
Thanuravi01 <thanuravi0109@gmail.com>
2023-05-06 09:15:08 +0530

    value of a changed

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
 
     public static void main(String[] args) {
-        int a=20;
+        int a=42;
         System.out.println("This was a file for test repo");
             }
 }
